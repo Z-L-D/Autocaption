af4ecec7297c51102930c48ea558123cd6634bbd
Z-L-D <zacldoyle@gmail.com>
2024-05-31 06:21:40 -0500

    -working on getting dataset viewer to work prior to release

--- autocaption_gradio.py
+++ autocaption_gradio.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import os
 import yaml
-from autocaption_util import main, parse_args
+from autocaption_util import chat, captioning, load_dataset, parse_args
 
 def load_config(config_path='config.yaml'):
     with open(config_path, 'r') as file:
@@ -26,20 +26,40 @@ def update_selected_llm(selected_llm):
     config['selected_llm'] = {'label': label, 'value': selected_llm}
     save_config(config)
 
-def run_script(input_dir, output_dir, llava_dir, batch_size, custom_prompt, keep_loaded):
+def run_chat_script(llm_dir, prompt, image, keep_loaded_chat):
+    # Prepare arguments
+    args = parse_args()
+    args.llm_dir = llm_dir
+    args.image = image
+    args.prompt = prompt
+    args.keep_loaded = keep_loaded_chat  # Pass the checkbox state to the args
+
+    chat_updates = chat(args)
+    for update in chat_updates:
+        yield update
+
+def run_captioning_script(input_dir, output_dir, llm_dir, batch_size, custom_prompt, keep_loaded_captioning):
     # Prepare arguments
     args = parse_args()
     args.input_dir = input_dir
     args.output_dir = output_dir
-    args.llava_dir = llava_dir
+    args.llm_dir = llm_dir
     args.batch_size = int(batch_size)
-    args.custom_prompt = custom_prompt
-    args.keep_loaded = keep_loaded  # Pass the checkbox state to the args
+    args.prompt = custom_prompt
+    args.keep_loaded = keep_loaded_captioning  # Pass the checkbox state to the args
     
-    # Run main function from the script and capture the generator output
-    progress_updates = main(args)  # main should be a generator now
+    # Run captioning function from the script and capture the generator output
+    progress_updates = captioning(args) 
     for update in progress_updates:
         yield update
+
+def load_samples(folder_path):
+    return load_dataset(folder_path)
+
+def display_sample(sample):
+    image_path, text = sample
+    print("Displaying image:", image_path)
+    return image_path, text
 
 # Disable Gradio Analytics
 gr.Blocks(analytics_enabled=False)
@@ -47,7 +67,8 @@ os.environ['GRADIO_ANALYTICS_ENABLED'] = 'False'
 
 # Define Gradio interface
 with gr.Blocks(title="Autocaption") as interface:
-    gr.Markdown("## Autocaption")
+    gr.Markdown("# Autocaption")
+    gr.Markdown("### v1.0")
 
     with gr.Row():
         llm_dir = gr.Dropdown(
@@ -56,31 +77,76 @@ with gr.Blocks(title="Autocaption") as interface:
             value=config['selected_llm']['value'],
             interactive=True
         )
-    with gr.Row():
-        progress_updates = gr.Label(label="Captioning Progress")
-    with gr.Row():
-        with gr.Column():
-            input_dir = gr.Textbox(label="Image Input Directory", placeholder="Path to your input directory containing images")
-            output_dir = gr.Textbox(label="Caption Output Directory", placeholder="Path to your output directory for text files")
+    with gr.Row(): 
+        with gr.Tab("Chat"):
             with gr.Row():
-                batch_size = gr.Slider(minimum=1, maximum=10, value=2, step=1, label="Batch Size")
-                keep_loaded = gr.Checkbox(label="Keep LLM Loaded After Captioning", value=True)  # Add the checkbox
-        with gr.Column():
-            with gr.Group():
-                custom_prompt = gr.Textbox(label="Description Prompt", placeholder="Enter your custom prompt here")
-                examples = gr.Examples(
-                    examples=[[prompt] for prompt in prompts],
-                    inputs=custom_prompt,
-                    label="Library Prompt",
-                    examples_per_page=6
-                )  
-    with gr.Row():
-        submit = gr.Button("Caption", elem_id="caption", variant="primary")
-    submit.click(
-        fn=run_script,
-        inputs=[input_dir, output_dir, llm_dir, batch_size, custom_prompt, keep_loaded],  # Include the checkbox in inputs
-        outputs=progress_updates,
-    ).success()
+                with gr.Column():                    
+                    with gr.Group():
+                        custom_prompt_chat = gr.Textbox(label="Your description prompt", placeholder="Type your description prompt here")
+                        examples = gr.Examples(
+                                examples=[[prompt] for prompt in prompts],
+                                inputs=custom_prompt_chat,
+                                label="Prompt Library",
+                                examples_per_page=6
+                            ) 
+                    keep_loaded_chat = gr.Checkbox(label="Keep LLM Loaded After Captioning", value=True)  # Add the checkbox
+                    submit_button = gr.Button("Submit")
+                    image = gr.Image(label="Upload Image", interactive=True)
+
+                with gr.Column():
+                    # chatbox = gr.Chatbot()
+                    chatbox = gr.Label(label="Image Caption")
+
+            submit_button.click(
+                fn=run_chat_script, 
+                inputs=[llm_dir, custom_prompt_chat, image, keep_loaded_chat], 
+                outputs=chatbox,
+            ).success()
+            
+        with gr.Tab("Batch Captioning"):
+            with gr.Row():
+                progress_updates = gr.Label(label="Captioning Progress")
+            with gr.Row():
+                with gr.Column():
+                    input_dir = gr.Textbox(label="Image Input Directory", placeholder="Path to your input directory containing images")
+                    output_dir = gr.Textbox(label="Caption Output Directory", placeholder="Path to your output directory for text files")
+                    with gr.Row():
+                        batch_size = gr.Slider(minimum=1, maximum=10, value=2, step=1, label="Batch Size")
+                        keep_loaded_captioning = gr.Checkbox(label="Keep LLM Loaded After Captioning", value=True)  # Add the checkbox
+                with gr.Column():
+                    with gr.Group():
+                        custom_prompt_captioning = gr.Textbox(label="Description Prompt", placeholder="Enter your custom prompt here")
+                        examples = gr.Examples(
+                            examples=[[prompt] for prompt in prompts],
+                            inputs=custom_prompt_captioning,
+                            label="Prompt Library",
+                            examples_per_page=6
+                        )  
+            with gr.Row():
+                submit = gr.Button("Caption", elem_id="caption", variant="primary")
+            submit.click(
+                fn=run_captioning_script,
+                inputs=[input_dir, output_dir, llm_dir, batch_size, custom_prompt_captioning, keep_loaded_captioning],  # Include the checkbox in inputs
+                outputs=progress_updates,
+            ).success()
+        
+        with gr.Tab("Dataset"):
+            folder_path_input = gr.Textbox(label="Dataset Folder Path", placeholder="Enter the path to the dataset folder")
+
+            load_button = gr.Button("Load Dataset")
+            
+            dataset = gr.Dataset(
+                components=[gr.Image(), gr.Textbox()],
+                samples=[],
+                headers=["Image", "Text"],
+                label="Dataset"
+            )
+        
+            image_display = gr.Image(label="Selected Image")
+            text_display = gr.Textbox(label="Selected Text", lines=10)
+        
+            load_button.click(fn=load_samples, inputs=folder_path_input, outputs=dataset, queue=True)
+            dataset.select(fn=display_sample, inputs=dataset, outputs=[image_display, text_display], queue=True)
 
     llm_dir.change(fn=update_selected_llm, inputs=llm_dir, outputs=None)
 
--- autocaption_util.py
+++ autocaption_util.py
@@ -2,13 +2,19 @@ import os
 import sys
 import argparse
 import json
+import io
+import numpy as np
 from PIL import Image
 import torch
-from transformers import (LlavaForConditionalGeneration, T5ForConditionalGeneration, LlamaForCausalLM, MistralForCausalLM, CLIPModel, AutoProcessor)
+from transformers import (LlavaForConditionalGeneration, AutoProcessor, LlavaProcessor, TextIteratorStreamer)
+import gradio as gr
+import time 
+from threading import Thread
 
 # Global variables to store the LLM and image processor
 text_encoder = None
 img_processor = None
+
 
 # Arguments
 def parse_args():
@@ -21,11 +27,6 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def batch_process_images(image_paths, img_processor, torch_device):
-    images = [Image.open(image_path) for image_path in image_paths]
-    prompts = ["A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\nDescribe the scene in detail, highlighting all the key elements, objects, and actions depicted in the image. Mention any explicit or potentially offensive content clearly with vulgarity if necessary. Use a minimum of 75 words and aim for a descriptive and engaging caption.\nASSISTANT:" for _ in images]
-    inputs = img_processor(prompts, images, return_tensors='pt', padding=True).to(torch_device)
-    return inputs
 
 def load_models(llava_dir):
     global text_encoder, img_processor
@@ -45,24 +46,105 @@ def load_models(llava_dir):
 
         img_processor = AutoProcessor.from_pretrained(llava_dir, torch_dtype=torch.float16)
 
+
 def unload_models():
     global text_encoder, img_processor
     text_encoder = None
     img_processor = None
     torch.cuda.empty_cache()  # Clear the GPU cache
 
-def main(args):
+
+def chat(args):
+    global text_encoder, img_processor
+    yield f'Loading Language Model...'
+    load_models(args.llm_dir)
+
+    prompt = args.prompt
+    image = args.image
+
+    print("Received prompt: ", prompt)
+    print("Received image type: ", type(image))
+
+    # Convert numpy array to file-like object if necessary
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(image.astype('uint8'), 'RGB')
+        print("Converted ndarray to PIL Image")
+    elif isinstance(image, bytes):
+        image = Image.open(io.BytesIO(image))
+        print("Converted bytes to PIL Image")
+    else:
+        image = Image.open(image)
+        print("Loaded image using PIL")
+
+    # Ensure the image is correctly loaded
+    if image is None:
+        print("Error: Image is None")
+        yield "Error: Image could not be processed."
+        return
+
+    prompts = [f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt}\nASSISTANT:"]
+
+    try:
+        inputs = img_processor(prompts, image, return_tensors='pt', padding=True).to("cuda")
+        print("Inputs processed for model")
+        yield f'Processing caption...'
+    except Exception as e:
+        print("Error during input processing: ", str(e))
+        yield f"Error during input processing: {str(e)}"
+        return
+    
+    try:
+        output = text_encoder.generate(**inputs, max_new_tokens=200, do_sample=False)
+        print("Model output generated")
+    except Exception as e:
+        print("Error during model generation: ", str(e))
+        yield f"Error during model generation: {str(e)}"
+        return
+
+    try:
+        decoded_output = img_processor.decode(output[0], skip_special_tokens=True)
+        print("Decoded output: " + decoded_output)
+        response = decoded_output.split('ASSISTANT: ')[-1].strip()
+        print("Response: " + response)
+        yield response
+    except Exception as e:
+        print("Error during output decoding: ", str(e))
+        yield f"Error during output decoding: {str(e)}"
+        return
+
+    # Unload models if not keeping them loaded
+    if not args.keep_loaded:
+        unload_models()
+        print("Models unloaded")
+
+
+def batch_process_images(image_paths, prompt, img_processor, torch_device):
+    images = [Image.open(image_path) for image_path in image_paths]
+    prompts = [f"A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: <image>\n{prompt}\nASSISTANT:" for _ in images]
+    inputs = img_processor(prompts, images, return_tensors='pt', padding=True).to(torch_device)
+    return inputs
+
+
+def captioning(args):
     global text_encoder, img_processor
     os.makedirs(args.output_dir, exist_ok=True)
 
     # Load models if not already loaded
-    load_models(args.llava_dir)
+    yield f'Loading Language Model...'
+    load_models(args.llm_dir)
+
+    prompt = args.prompt
 
     # List all image files and count text files
-    all_image_files = [os.path.join(args.input_dir, f) for f in os.listdir(args.input_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
-    total_images = len(all_image_files)
-    text_files = [os.path.join(args.output_dir, f) for f in os.listdir(args.output_dir) if f.lower().endswith('.txt')]
-    processed_images = len(text_files)
+    try:
+        all_image_files = [os.path.join(args.input_dir, f) for f in os.listdir(args.input_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
+        total_images = len(all_image_files)
+        text_files = [os.path.join(args.output_dir, f) for f in os.listdir(args.output_dir) if f.lower().endswith('.txt')]
+        processed_images = len(text_files)
+    except Exception as e:
+        print("Error during file listing: ", str(e))
+        yield f"Error during file listing: {str(e)}"
+        return
 
     # Display initial progress
     initial_progress = (processed_images / total_images) * 100
@@ -79,23 +161,56 @@ def main(args):
         if not batch_files:
             continue
 
-        inputs = batch_process_images(batch_files, img_processor, "cuda")
-        outputs = text_encoder.generate(**inputs, max_new_tokens=200, do_sample=False)
+        try:
+            inputs = batch_process_images(batch_files, prompt, img_processor, "cuda")
+            print("Inputs processed for model")
+        except Exception as e:
+            print("Error during input processing: ", str(e))
+            yield f"Error during input processing: {str(e)}"
+            return
+
+        try:
+            outputs = text_encoder.generate(**inputs, max_new_tokens=200, do_sample=False)
+            print("Model output generated")
+        except Exception as e:
+            print("Error during model generation: ", str(e))
+            yield f"Error during model generation: {str(e)}"
+            return
+
         for idx, output in enumerate(outputs):
-            decoded_output = img_processor.decode(output[2:], skip_special_tokens=True)
-            assistant_response = decoded_output.split('ASSISTANT: ')[-1].strip()
-            output_filename = os.path.splitext(os.path.basename(batch_files[idx]))[0] + '.txt'
-            output_path = os.path.join(args.output_dir, output_filename)
-            with open(output_path, 'w') as file:
-                file.write(assistant_response)
-            processed_images += 1
-            progress = (processed_images / total_images) * 100
-            yield f'Processed {processed_images}/{total_images} images ({progress:.2f}%)'
+            try:
+                decoded_output = img_processor.decode(output[2:], skip_special_tokens=True)
+                assistant_response = decoded_output.split('ASSISTANT: ')[-1].strip()
+                output_filename = os.path.splitext(os.path.basename(batch_files[idx]))[0] + '.txt'
+                output_path = os.path.join(args.output_dir, output_filename)
+                with open(output_path, 'w') as file:
+                    file.write(assistant_response)
+                print(f"Processed file: {output_path}")
+                processed_images += 1
+                progress = (processed_images / total_images) * 100
+                yield f'Processed {processed_images}/{total_images} images ({progress:.2f}%)'
+            except Exception as e:
+                print("Error during output decoding or saving: ", str(e))
+                yield f"Error during output decoding or saving: {str(e)}"
+                return
 
     # Unload models if not keeping them loaded
     if not args.keep_loaded:
         unload_models()
+        print("Models unloaded")
 
-if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+
+def load_dataset(folder_path):
+    samples = []
+    image_extensions = ('.png', '.jpg', '.jpeg', '.bmp', '.gif')
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(image_extensions):
+            image_path = os.path.join(folder_path, filename)
+            image_path = os.path.normpath(image_path)  # Normalize the path
+            text_path = os.path.join(folder_path, os.path.splitext(filename)[0] + '.txt')
+            if os.path.exists(text_path):
+                with open(text_path, 'r') as file:
+                    text = file.read()
+                samples.append([image_path, text])
+    print("Loaded samples:", samples)
+    return samples
